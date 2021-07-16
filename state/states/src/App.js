bb7ebf331a7492c67dec722f015d1991e78539fa
Umit Turk <uyt.turk@gmail.com>
2021-07-16 21:05:26 +0300

    lifecycle

--- state/states/src/App.js
+++ state/states/src/App.js
@@ -1,38 +1,39 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, SafeAreaView, Text, View, FlatList, Switch} from 'react-native';
 
-const data = [
-  {id: 0, name: 'cafe.exe', isFavorite: true},
-  {id: 1, name: 'KafaKafe', isFavorite: false},
-  {id: 2, name: "Rock'n Code", isFavorite: true},
-  {id: 3, name: 'do(drink)', isFavorite: false},
-  {id: 4, name: 'BugG', isFavorite: true},
-  {id: 5, name: 'esc', isFavorite: false},
-];
+
+
 
 const App = () => {
-  const [cafeList, setCafeList] = useState(data);
-  const [showOnlyFavorities, setShowOnlyFavorities] = useState(false);
+ 
+  const [helloFlag, setHelloFlag] = useState(true)
 
-  function onFavoritesChange(isFavoriteSelected) {
-    setShowOnlyFavorities(isFavoriteSelected);
-    isFavoriteSelected
-      ? setCafeList(cafeList.filter(cafe => cafe.isFavorite))
-      : setCafeList(data);
+  function updateFlag()  {
+    setHelloFlag(!helloFlag)
   }
 
   return (
     <SafeAreaView>
-      <View>
-        <Text>Show only favorities</Text>
-        <Switch value={showOnlyFavorities} onValueChange={onFavoritesChange} />
-      </View>
-      <FlatList
-        data={cafeList}
-        renderItem={({item}) => <Text>{item.name}</Text>}
-      />
+      <Text>Hello Lifecycle</Text>
+      <Button title="Up!" onPress={updateFlag} />
+    {helloFlag && <Hello /> }
     </SafeAreaView>
   );
 };
 
 export default App;
+
+function Hello(){
+
+  useEffect(() => {
+    console.log("merhaba dünya");
+  
+    return () => {
+      console.log("gidiyorum bütün aşklar yüreğimde")
+    }
+  }, []);
+
+  return (
+    <Text>I'm hello component</Text>
+  )
+}
