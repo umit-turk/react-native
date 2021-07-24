f5af56b6e5b9b97e21a9cdb154edfc28392c8222
Umit Turk <uyt.turk@gmail.com>
2021-07-24 15:27:19 +0300

    lottie animation

--- Dukkan/dukkan/src/pages/Products/Products.js
+++ Dukkan/dukkan/src/pages/Products/Products.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import { Text, View, FlatList, ActivityIndicator } from 'react-native';
-import Config from "react-native-config";
+import {Text, View, FlatList, ActivityIndicator} from 'react-native';
+import Config from 'react-native-config';
 import config from '../../../config';
+import Loading from '../../components/Loading';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import useFetch from '../../hooks/useFetch/useFetch';
-
-
+import Error from '../../components/Error';
 function Products() {
-    const {loading, data, error} = useFetch(config.API_URL);
+  const {loading, data, error} = useFetch(config.API_URL);
 
-    const renderProduct = ({item}) => <ProductCard product={item} />
+  const renderProduct = ({item}) => <ProductCard product={item} />;
 
-    if(loading){
-        return <ActivityIndicator size="large" />
+  if(loading){
+      return <Loading />;
     }
     if(error){
-      return  <Text>{error}</Text>
+      return  <Error />;
     }
 
     return (
@@ -29,4 +29,4 @@ function Products() {
     )
 }
 
-export default Products
+export default Products;
