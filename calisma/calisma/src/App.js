bb19417941e570c3e8809fecf37f9c6c38b33ce5
Umit Turk <uyt.turk@gmail.com>
2021-07-30 23:26:18 +0300

    logout

--- calisma/calisma/src/App.js
+++ calisma/calisma/src/App.js
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from './pages/auth/Login/Login';
 import Sign from './pages/auth/Sign/Sign';
 import 'react-native-gesture-handler';
-import FlashMessage from "react-native-flash-message";
+import FlashMessage from 'react-native-flash-message';
 import Messages from './pages/Messages/Messages';
-
+import auth from '@react-native-firebase/auth';
+import {Text, View} from 'react-native';
 
 const Stack = createStackNavigator();
 
- export default () => {
+export default () => {
+  const [userSession, setUserSession] = useState();
+
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      setUserSession(!!user);
+    });
+  }, []);
+
   const AuthStack = () => {
     return (
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="LoginPage" component={Login} />
         <Stack.Screen name="SignPage" component={Sign} />
-        
       </Stack.Navigator>
     );
   };
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen 
-        name="MessagesScreen"
-        component={Messages}
-        />
-        <Stack.Screen name="AuthStack" component={AuthStack} />
-      </Stack.Navigator>
+      {!userSession ? (
+        AuthStack()
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MessagesScreen"
+            component={Messages}
+            options={{
+              headerRight: () => (
+                <View>
+                  <Text onPress={() => auth().signOut()}>Logout</Text>
+                </View>
+              ),
+            }}
+          />
+        </Stack.Navigator>
+      )}
       <FlashMessage position="top" />
     </NavigationContainer>
   );
 };
-
