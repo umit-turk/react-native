78db0d87fb44dc580705ebb5a9b6457b79167d74
Umit Turk <uyt.turk@gmail.com>
2021-07-30 20:37:15 +0300

    content göndermek

--- calisma/calisma/src/pages/Messages/Messages.js
+++ calisma/calisma/src/pages/Messages/Messages.js
@@ -1,23 +1,51 @@
-import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, FlatList} from 'react-native';
 import FloatingButton from '../../components/FloatingButton';
 import styles from './Messages.style';
 import ContentInput from '../../components/modal/ContentInput';
 
+import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth';
+
 const Messages = () => {
   const [inputModalVisible, setInputModalVisible] = useState(false);
+
+  //Flatlist'te göstereceğim datalar
+  const [contentList, setContentList] = useState([]);
+
+  useEffect(() => {
+    database()
+      .ref('/messages/')
+      .on('value', snapshot => {
+        const contentData = snapshot.val()
+        console.log(contentData);
+      });
+  }, []);
 
   function handleInputToggle() {
     setInputModalVisible(!inputModalVisible);
   }
 
   function handleSendContent(content) {
-      handleInputToggle();
-      console.log(content)
+    handleInputToggle();
+    sendContent(content);
+    console.log(content);
+  }
+
+  function sendContent(content) {
+    const userMail = auth().currentUser.email;
+
+    const contentObj = {
+      text: content,
+      username: userMail.split('@')[0],
+      date: new Date().toISOString(),
+    };
+    database().ref('/messages/').push(contentObj);
   }
 
   return (
     <View style={styles.container}>
+      <FlatList />
       <FloatingButton onPress={handleInputToggle} />
       <ContentInput
         visible={inputModalVisible}
